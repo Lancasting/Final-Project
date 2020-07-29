b9bedaf9b601d911d0715f3d91be03953c677179
ike barranco <barranco.ike@outlook.com>
2020-07-29 04:08:43 -0600

    added and installed stuff for user authentication

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //Dependencies
 const express = require("express");
+const session = require("express-session");
 const compression = require("compression");//Performance
 const mongoose = require("mongoose");//Database
+const passport = require("./config/passport.js");
 require("dotenv").config()//.env use
 const path = require("path");//Grab Paths
 const PORT = process.env.PORT || 3001;//Port#
@@ -23,11 +25,18 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ticketing_syste
 //user compression for performance
 app.use(compression());
 
-//Add future routes here
-/**
- * app.use(Routes)
- */
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: process.env.SECRET || "secret", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Add future routes here
+app.use(require("./routes/userRoutes"));
 
 // Send every request to the React app
 // Define any API routes before this runs

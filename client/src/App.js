ef17cb41d3a683f0a9cb325619ed93a7e13985af
ike barranco <barranco.ike@outlook.com>
2020-07-29 02:01:28 -0600

    added Router for application

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
     );
   }
 }

b058d55ab38a6421e2d931dbaefb1e1c2c4bd35f
ike barranco <barranco.ike@outlook.com>
2020-07-29 01:45:51 -0600

    Added mongoose npm and connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const compression = require("compression");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,8 +10,23 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+//Connect To Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ticketing_system", error =>{
+  //Check if connection error
+  if(error) return console.log("Connection Unsuccessful");
+  //Log Successfull connection
+  console.log("Connection Successful");
+});
+
+//user compression for performance
 app.use(compression());
 
+//Add future routes here
+/**
+ * 
+ */
+
+ 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {

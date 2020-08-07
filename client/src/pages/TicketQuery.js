ad3cc31d529718aca09a0d1cda583dd413f7c8d1
ike barranco <barranco.ike@outlook.com>
2020-08-07 00:02:23 -0600

    added helmet to ticket query page

--- client/src/pages/TicketQuery.js
+++ client/src/pages/TicketQuery.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function TicketQuery() {
-  return <>UserProfile</>;
+  return (
+    <>
+      <Helmet>
+        <title>HALP - Ticket Page</title>
+        <meta name="description" content="Ticket Page Of The HALP Website" />
+      </Helmet>
+    </>
+  );
 }
 
 export default TicketQuery;

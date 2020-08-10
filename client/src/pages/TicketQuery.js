65fad4d470115bd802b705f6234c7c77820d7d6e
ike barranco <barranco.ike@outlook.com>
2020-08-09 20:23:54 -0600

    added navbar to the login page;

--- client/src/pages/TicketQuery.js
+++ client/src/pages/TicketQuery.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import TicketNavbar from "../components/TicketNavbar.js";
 import TicketQueryForm from "../components/TicketQueryForm.js";
 import Navbar from "../components/NavBar.js";
 // import TicketSummary from "../components/TicketSummary.js";

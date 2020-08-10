0a76e416750ba6de52032fd8b81e574c32d6126d
ike barranco <barranco.ike@outlook.com>
2020-08-10 17:09:40 -0600

    added information to the ticket summary

--- client/src/components/TicketSummary.js
+++ client/src/components/TicketSummary.js
@@ -2,22 +2,26 @@ import React from "react";
 import { List, Grid, Segment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-function TicketSummary() {
+function TicketSummary({ _id, priorityLevel, createBy, status }) {
   return (
     <>
       <List.Item as={Link} to="/" style={{ color: "black" }}>
         <Grid as={Segment}>
           <Grid.Column mobile={16} tablet={8} computer={4}>
-            Ticket Number
+            <p>Ticket</p>
+            <p>{_id}</p>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={8} computer={4}>
-            Ticket Number
+            <p>Status</p>
+            <p>{status}</p>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={8} computer={4}>
-            Ticket Number
+            <p>Priority</p>
+            <p>{priorityLevel}</p>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={8} computer={4}>
-            Ticket Number
+            <p>Created By</p>
+            <p>{createBy}</p>
           </Grid.Column>
         </Grid>
       </List.Item>

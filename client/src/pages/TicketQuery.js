b4fc62c8ec3919a56e3340b56cb791118e66c0d1
ike barranco <barranco.ike@outlook.com>
2020-08-11 21:31:22 -0600

    imported sidebar to ticket query page

--- client/src/pages/TicketQuery.js
+++ client/src/pages/TicketQuery.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/API.js";
+import SideBar from "../components/SideBar.js";
 import TicketQueryForm from "../components/TicketQueryForm.js";
 import Navbar from "../components/NavBar.js";
 import TicketSummary from "../components/TicketSummary.js";
 import { Helmet } from "react-helmet";
-import {
-  Sidebar,
-  Menu,
-  List,
-  Button,
-  Sticky,
-  Icon,
-  // Rail,
-} from "semantic-ui-react";
+import { List, Button, Icon } from "semantic-ui-react";
 
 function TicketQuery() {
-  // state of query
-  const [visible, setVisible] = useState(false);
+  // state of query\
   const [tickets, setTickets] = useState([]);
   const [query, setQuery] = useState("all");
 
@@ -38,42 +30,15 @@ function TicketQuery() {
         <title>HALP - Ticket Page</title>
         <meta name="description" content="Ticket Page Of The HALP Website" />
       </Helmet>
-      <Navbar loggedIn={true} />
-      <Sidebar.Pushable>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          icon="labeled"
-          onHide={() => setVisible(false)}
-          inverted
-          vertical
-          visible={visible}
-          width="thin"
-        >
-          <Menu.Item as="a">Search</Menu.Item>
-          <Menu.Item as="a">Projects</Menu.Item>
-          <Menu.Item as="a">Create</Menu.Item>
-        </Sidebar>
-        <Sidebar.Pusher dimmed={visible}>
-          <Sticky>
-            <Button
-              icon
-              labelPosition="right"
-              onClick={() => setVisible(true)}
-              style={{ zIndex: 1 }}
-            >
-              Menu
-              <Icon name="right arrow" />
-            </Button>
-          </Sticky>
-          <TicketQueryForm />
-          <List>
-            {tickets.map((ticket) => (
-              <TicketSummary key={ticket._id} {...ticket} />
-            ))}
-          </List>
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
+      <SideBar>
+        <Navbar loggedIn={true} />
+        <TicketQueryForm />
+        <List>
+          {tickets.map((ticket) => (
+            <TicketSummary key={ticket._id} {...ticket} />
+          ))}
+        </List>
+      </SideBar>
     </>
   );
 }

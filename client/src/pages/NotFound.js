da2d105ce36c3de56ef3b9f604283829ca0b1ae1
ike barranco <barranco.ike@outlook.com>
2020-08-14 19:00:59 -0600

    removed unused items from not found page

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,8 +1,7 @@
 import React from "react";
-import NavBar from "../components/NavBar.js";
 import { Helmet } from "react-helmet";
 
-function NotFound({ loggedIn }) {
+function NotFound() {
   return (
     <>
       <Helmet>

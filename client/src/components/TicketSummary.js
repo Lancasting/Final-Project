43089bb6aa41436b251edeb9946a41900c042841
ike barranco <barranco.ike@outlook.com>
2020-08-16 18:40:21 -0600

    changes to ticket summary page

--- client/src/components/TicketSummary.js
+++ client/src/components/TicketSummary.js
@@ -1,36 +1,32 @@
 import React from "react";
-import { List, Grid, Segment } from "semantic-ui-react";
+import { List, Segment, Header, Grid } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 function TicketSummary({ _id, priorityLevel, createdBy, status }) {
   return (
-    <>
-      <List.Item style={{ color: "black", padding: "10px" }}>
-        <Grid
-          as={Segment}
-          style={{ marginTop: 0, marginBottom: 0 }}
-          columns="equal"
-        >
-          <Grid.Column>
-            <p style={{ fontWeight: "bolder" }}>Ticket Number</p>
+    <Segment style={{ color: "black", margin: "10px" }}>
+      <List>
+        <Grid>
+          <Grid.Column mobile={16} tablet={8} computer={4}>
+            <Header as="h3" content="Ticket:" />
             <Link to={`/tickets/${_id}`}>{_id}</Link>
           </Grid.Column>
-          <Grid.Column>
-            <p style={{ fontWeight: "bolder" }}>Priority</p>
-            <p>{priorityLevel}</p>
-          </Grid.Column>
-          <Grid.Column>
-            <p style={{ fontWeight: "bolder" }}>Status</p>
+          <Grid.Column mobile={16} tablet={8} computer={4}>
+            <Header as="h3" content="Status:" />
             <p>{status}</p>
           </Grid.Column>
-          <Grid.Column>
-            <p style={{ fontWeight: "bolder" }}>Created By</p>
-            <p>Email: {createdBy.email}</p>
-            <p>ID: {createdBy._id}</p>
+          <Grid.Column mobile={16} tablet={8} computer={4}>
+            <Header as="h3" content="Priority Level:" />
+            <p>{priorityLevel}</p>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={8} computer={4}>
+            <Header as="h3" content="Created By:" />
+            <p>{createdBy.email}</p>
+            <p>{createdBy._id}</p>
           </Grid.Column>
         </Grid>
-      </List.Item>
-    </>
+      </List>
+    </Segment>
   );
 }
 

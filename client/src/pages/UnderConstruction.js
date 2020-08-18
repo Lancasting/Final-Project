26471302df040e59af721a2cc2aed02958bdee23
Ike <barranco.ike@gmail.com>
2020-08-18 01:16:03 -0600

    added material to the page not found file

--- client/src/pages/UnderConstruction.js
+++ client/src/pages/UnderConstruction.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Container, Header, Image } from "semantic-ui-react";
+import { Container, Image } from "semantic-ui-react";
 import "./underconstruction.css";
 
 function UnderConstruction() {
@@ -13,11 +13,11 @@ function UnderConstruction() {
           content="Page Not Available form the HALP website"
         />
       </Helmet>
-      <Container textAlign="center">
+      <Container textAlign="center" style={{ height: "100%" }}>
         <Image
           src="/images/constructionImage.png"
           alt="Construction sign made by josethestoryteller https://pixabay.com/illustrations/under-construction-construction-sign-2408059/"
-          size="meduim"
+          size="large"
           circular
           centered
         />
